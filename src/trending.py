dd30282ce5b8dea2083e24c27f3f0f6584de9022
Mark Mahacek <mmahacek@opennms.com>
2022-02-10 21:43:52 -0800

    actually match in and out metrics on the graph data

--- src/trending.py
+++ src/trending.py
@@ -1,4 +1,5 @@
-# trending
+# trending.py
+# cspell:ignore tozeroy
 
 from datetime import datetime
 from models import Day
@@ -7,33 +8,39 @@ from ra_processing import average_metrics
 import plotly.express as px
 import plotly.graph_objects as go
 
-def time_trend(parsed_metrics:dict, interface:str, label:list=['ifOutOctets','ifInOctets']) -> dict:
+def time_trend(parsed_metrics:dict, interface:str, label:list) -> dict:
     trend = {'x': [], 'y': [], 'z': [], 'c': [], 'hour':{}}
     for j in parsed_metrics[interface]['day_of_week']:
         if j != 'total':
             for k in parsed_metrics[interface]['day_of_week'][j]:
-                if label[0] in parsed_metrics[interface]['day_of_week'][j][k]:
-                    if parsed_metrics[interface]['day_of_week'][j][k][label[0]] != None:
-                        trend['x'].append(Day(j).name)
-                        if k == 'total':
-                            trend['y'].append(f'average')
-                        else:
-                            trend['y'].append(f'{k}:00')
-                        trend['z'].append(parsed_metrics[interface]['day_of_week'][j][k][label[0]])
-                        trend['c'].append('Bytes Out')
-                if  label[1] in parsed_metrics[interface]['day_of_week'][j][k]:
-                    if parsed_metrics[interface]['day_of_week'][j][k][label[1]] != None:
-                        trend['x'].append(Day(j).name)
-                        if k == 'total':
-                            trend['y'].append(f'average')
-                        else:
-                            trend['y'].append(f'{k}:00')
-                        trend['z'].append(parsed_metrics[interface]['day_of_week'][j][k][label[1]])
-                        trend['c'].append('Bytes In')
+                for metric in label:
+                    if 'out' in metric.lower():
+                        if metric in parsed_metrics[interface]['day_of_week'][j][k]:
+                            if parsed_metrics[interface]['day_of_week'][j][k][metric] != None:
+                                trend['x'].append(Day(j).name)
+                                if k == 'total':
+                                    trend['y'].append(f'average')
+                                else:
+                                    trend['y'].append(f'{k}:00')
+                                trend['z'].append(parsed_metrics[interface]['day_of_week'][j][k][metric] * 8)
+                                trend['c'].append('Bytes Out')
+                    elif 'in' in metric.lower():
+                        if metric in parsed_metrics[interface]['day_of_week'][j][k]:
+                            if parsed_metrics[interface]['day_of_week'][j][k][metric] != None:
+                                trend['x'].append(Day(j).name)
+                                if k == 'total':
+                                    trend['y'].append(f'average')
+                                else:
+                                    trend['y'].append(f'{k}:00')
+                                trend['z'].append(parsed_metrics[interface]['day_of_week'][j][k][metric] * 8)
+                                trend['c'].append('Bytes In')
     for k in parsed_metrics[interface]['hour_of_day']:
         trend['hour'][k] = {'Bytes Out': [], 'Bytes In': []}
-        trend['hour'][k]['Bytes Out'].append(parsed_metrics[interface]['hour_of_day'][k].get(label[0]))
-        trend['hour'][k]['Bytes In'].append(parsed_metrics[interface]['hour_of_day'][k].get(label[1]))
+        for metric in label:
+            if 'out' in metric.lower():
+                trend['hour'][k]['Bytes Out'].append(parsed_metrics[interface]['hour_of_day'][k].get(metric, 0) * 8)
+            elif 'in' in metric.lower():
+                trend['hour'][k]['Bytes In'].append(parsed_metrics[interface]['hour_of_day'][k].get(metric, 0) * 8)
     for k in parsed_metrics[interface]['hour_of_day']:
         trend['hour'][k] = average_metrics(trend['hour'][k])
     for k in trend['hour']:
@@ -78,41 +85,55 @@ def find_weekends(parsed_metrics:dict, interface:str) -> list:
     return weekends
 
 
-def time_lines(parsed_metrics:dict, interface:str, label:list=['ifOutOctets','ifInOctets']) -> dict:
-    stats = {'x': [], 'y': []}
-    stats2 = {'x': [], 'y': []}
+def time_lines(parsed_metrics:dict, interface:str, label:list) -> dict:
+    stats_out = {'x': [], 'y': []}
+    stats_in = {'x': [], 'y': []}
     for i in parsed_metrics:
         if i == interface:
-            #print(parsed_metrics[i]['day_of_week'])
             for j in parsed_metrics[i]['ts']:
                 date = datetime.fromtimestamp(j/1000)
-                stats['x'].append(date)
-                stats2['x'].append(date)
-                if parsed_metrics[i]['ts'][j][label[0]] not in [None, [None]]:
-                    stats['y'].append(parsed_metrics[i]['ts'][j].get(label[0]))
-                else:
-                    stats['y'].append(0)
-                if parsed_metrics[i]['ts'][j].get(label[1]) not in [None, [None]]:
-                    stats2['y'].append(parsed_metrics[i]['ts'][j].get(label[1]) * -1)
-                else:
-                    stats2['y'].append(0)
-                #stats['c'].append('BytesOut')
-                #stats['c'].append('BytesIn')
-        if len(stats['x']) >= 100:
+                for metric in label:
+                    if 'out' in metric.lower():
+                        if parsed_metrics[i]['ts'][j].get(metric) not in [None, [None]]:
+                            stats_out['y'].append(parsed_metrics[i]['ts'][j].get(metric) * -8)
+                        else:
+                            stats_out['y'].append(0)
+                        stats_out['x'].append(date)
+                    elif 'in' in metric.lower():
+                        stats_in['x'].append(date)
+                        if parsed_metrics[i]['ts'][j].get(metric) not in [None, [None]]:
+                            stats_in['y'].append(parsed_metrics[i]['ts'][j].get(metric) * 8)
+                        else:
+                            stats_in['y'].append(0)
+        if len(stats_out['x']) >= 100:
             break
-    return stats, stats2
+    return stats_out, stats_in
+
+#theme = {'out': 'rgba(165, 50, 94,1)', 'in': 'rgba(20, 209, 223,1)', 'weekend': 'rgba(126,129,157,1)'}
+theme = {'out': '#204a87', 'in': '#4e9a06', 'weekend': 'rgba(126,129,157,1)', 'outfill': '#3465a4', 'infill': '#73d216'}
 
 def get_trend_graph(trend:dict) -> px.scatter:
-    fig = px.scatter(x=trend['x'], y=trend['y'], size=trend['z'], color=trend['c'], labels={'x': 'Day of Week', 'y': 'Time', 'size': 'Bytes', 'color': 'Metric'})
+    fig = px.scatter(x=trend['x'], y=trend['y'], size=trend['z'], color=trend['c'], labels={'x': 'Day of Week', 'y': 'Time', 'size': 'Bytes', 'color': 'Metric'}, color_discrete_map={"Bytes Out": theme['out'], "Bytes In": theme['in']})
     fig.add_shape(y0="8:00", y1="8:00", x0=-.5, x1=7.5, type="line", line_color="black", line_width=1)
-    fig.add_shape(y0="18:00", y1="18:00", x0=-.5, x1=7.5, type="line", line_color="black", line_width=1)
-    fig.add_vrect(x0=4.5, x1=6.5, fillcolor="rgba(255,85,94,1)", opacity=0.25, layer="below", line_width=0)
+    if '17:00' in trend['y']:
+        fig.add_shape(y0="17:00", y1="17:00", x0=-.5, x1=7.5, type="line", line_color="black", line_width=1)
+    else:
+        fig.add_shape(y0="18:00", y1="18:00", x0=-.5, x1=7.5, type="line", line_color="black", line_width=1)
+    fig.add_vrect(x0=4.5, x1=6.5, fillcolor=theme['weekend'], opacity=0.25, layer="below", line_width=0)
     return fig
 
-def get_trend_line(stats:dict, stats2:dict, weekends:dict) -> go.Figure:
+def get_trend_line(stats_out:dict, stats_in:dict, weekends:dict) -> go.Figure:
     fig2 = go.Figure()
-    fig2.add_trace(go.Scatter(x=stats['x'], y=stats['y'], mode="lines", name="BytesOut", line={'color':"rgba(165, 50, 94,1)"}))
-    fig2.add_trace(go.Scatter(x=stats2['x'], y=stats2['y'], mode="lines", name="BytesIn", line={'color':"rgba(20, 209, 223,1)"}))
+    fig2.add_trace(go.Scatter(x=stats_out['x'], y=stats_out['y'], mode="lines", name="BytesOut", line={'color':theme['out']}, fill='tozeroy', fillcolor=theme['outfill']))
+    fig2.add_trace(go.Scatter(x=stats_in['x'], y=stats_in['y'], mode="lines", name="BytesIn", line={'color':theme['in']}, fill='tozeroy', fillcolor=theme['infill']))
     for weekend in weekends:
-        fig2.add_vrect(x0=weekend[0], x1=weekend[1], fillcolor="rgba(126,129,157,1)", opacity=0.25, layer="below", line_width=0)
+        fig2.add_vrect(x0=weekend[0], x1=weekend[1], fillcolor=theme['weekend'], opacity=0.25, layer="below", line_width=0)
     return fig2
+
+# area rawbitsIn #73d216
+# line rawbitsIn #4e9a06
+# area rawbitsOutNeg #3465a4
+# line rawbitsOutNeg #204a87
+
+#AREA #00ff00
+#AREA bitsOutNeg #0000ff
