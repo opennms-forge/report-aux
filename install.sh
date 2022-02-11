b073cca6f1ab682ddc5b161999947e75f0285c26
Mark Mahacek <mmahacek@opennms.com>
2022-02-10 23:28:35 -0800

    install service file

--- install.sh
+++ install.sh
@@ -2,3 +2,5 @@ python3 -m venv venv
 source venv/bin/activate
 pip3 install --upgrade pip wheel
 pip3 install -r requirements.txt
+sudo cp report-aux.service /etc/systemd/system/
+sudo systemctl daemon-reload
