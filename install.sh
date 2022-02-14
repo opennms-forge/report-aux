6dcf59ae90a7eb0572cca279b7cb8fe283c1f67e
Mark Mahacek <mmahacek@opennms.com>
2022-02-14 09:50:09 -0800

    update readme

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
+#!env bash
+# @author Mark Mahacek <mmahacek@opennms.com>
+
 python3 -m venv venv
 source venv/bin/activate
 pip3 install --upgrade pip wheel
 pip3 install -r requirements.txt
-sudo cp report-aux.service /etc/systemd/system/
-sudo systemctl daemon-reload
